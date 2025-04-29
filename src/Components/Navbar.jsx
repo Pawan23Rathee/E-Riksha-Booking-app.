1be9a15ff2efb79139fd9a1a4b8ecca50747b5dd
Pawan Rathee <ratheepawan666@gmail.com>
2025-04-29 11:17:28 +0530

    Start Making Navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav>
+        <img src="" alt="" />
+
+
+
+        <ul>
+            <li>Home</li>
+            <li>History</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
