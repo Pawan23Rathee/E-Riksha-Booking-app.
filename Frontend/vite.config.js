16338af89f1af254cce5d5d795e6826124697796
Pawan Rathee <ratheepawan666@gmail.com>
2025-05-24 16:46:39 +0530

    Deploying

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // 👈 This is the key change
+    port: process.env.PORT || 5173, // optional
+  }
+});
